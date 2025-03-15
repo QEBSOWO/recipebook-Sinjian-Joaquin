f77294de23ff388951122613107176c3ba730a3c
QEBSOWO <enrique.sinjian@student.ateneo.edu>
2025-03-15 21:47:47 +0800

    changed views from def to class based

--- ledger/views.py
+++ ledger/views.py
@@ -1,160 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .models import Ingredient, Recipe, RecipeIngredient
 
 # Create your views here.
 
-'''
-recipe1 = {
-    "name": "Recipe 1",
-    "ingredients": [
-        {
-            "name": "tomato",
-            "quantity": "3pcs"
-        },
-        {
-            "name": "onion",
-            "quantity": "1pc"
-        },
-        {
-            "name": "pork",
-            "quantity": "1kg"
-        },
-        {
-            "name": "water",
-            "quantity": "1L"
-        },
-        {
-            "name": "sinigang mix",
-            "quantity": "1 packet"
-        }
-    ],
-    "link": "/recipe/1"
-}
+class recipe_list_view(ListView):
 
-recipe2 = {
-    "name": "Recipe 2",
-    "ingredients": [
-        {
-            "name": "garlic",
-            "quantity": "1 head"
-        },
-        {
-            "name": "onion",
-            "quantity": "1pc"
-        },
-        {
-            "name": "vinegar",
-            "quantity": "1/2cup"
-        },
-        {
-            "name": "water",
-            "quantity": "1 cup"
-        },
-        {
-            "name": "salt",
-            "quantity": "1 tablespoon"
-        },
-        {
-            "name": "whole black peppers",
-            "quantity": "1 tablespoon"
-        },
-        {
-            "name": "pork",
-            "quantity": "1 kilo"
-        }
-    ],
-    "link": "/recipe/2"
-}
+    model = Recipe
+    template_name = 'recipe_list'
 
-recipes = {
-    "recipes": [
-        {
-            "name": "Recipe 1",
-            "ingredients": [
-                {
-                    "name": "tomato",
-                    "quantity": "3pcs"
-                },
-                {
-                    "name": "onion",
-                    "quantity": "1pc"
-                },
-                {
-                    "name": "pork",
-                    "quantity": "1kg"
-                },
-                {
-                    "name": "water",
-                    "quantity": "1L"
-                },
-                {
-                    "name": "sinigang mix",
-                    "quantity": "1 packet"
-                }
-            ],
-            "link": "/recipe/1"
-        },
-        {
-            "name": "Recipe 2",
-            "ingredients": [
-                {
-                    "name": "garlic",
-                    "quantity": "1 head"
-                },
-                {
-                    "name": "onion",
-                    "quantity": "1pc"
-                },
-                {
-                    "name": "vinegar",
-                    "quantity": "1/2cup"
-                },
-                {
-                    "name": "water",
-                    "quanity": "1 cup"
-                },
-                {
-                    "name": "salt",
-                    "quantity": "1 tablespoon"
-                },
-                {
-                    "name": "whole black peppers",
-                    "quantity": "1 tablespoon"
-                },
-                {
-                    "name": "pork",
-                    "quantity": "1 kilo"
-                }
-            ],
-            "link": "/recipe/2"
-        }
-    ]
-}
-'''
 
-def recipe_list_view(request):
-    print(request)
+class recipe_detail_view(LoginRequiredMixin, DetailView):
+    
+    model = Recipe
+    template_name = 'recipe_detail.html'
+    redirect_field_name = ''
 
-    recipes = Recipe.objects.all()
-    ingredients = Ingredient.objects.all()
-
-    ctx = {
-        "Recipes": recipes,
-        "Ingredients": ingredients
-    }
-
-    return render(request, "recipe_list.html", ctx)
-
-def recipe_detail_view(request, pk):
-    print(request)
-
-    recipe = Recipe.objects.get(pk = pk)
-
-    ctx = {
-        "Recipe": recipe,
-    }
-
-    return render(request, "recipe_detail.html", ctx)

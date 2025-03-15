b324d0dc0d7e7de587ff6394a6d3a1aa9ff09e2b
QEBSOWO <enrique.sinjian@student.ateneo.edu>
2025-03-15 21:32:36 +0800

    Updated Recipe Model

--- ledger/models.py
+++ ledger/models.py
@@ -1,16 +1,31 @@
 from django.db import models
 from django.urls import reverse
 
+from django.contrib.auth.models import User
+
 # Create your models here.
 
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True)
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+    
+    
 class Recipe(models.Model):
     name = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
     
     def get_absolute_url(self):
         return reverse('ledger:recipe_detail', args=[self.pk])
+
 
 class Ingredient(models.Model):
     name = models.CharField(max_length=100)

05908e7f248fc4d50b4cde2c26059f6141d34a87
QEBSOWO <enrique.sinjian@student.ateneo.edu>
2025-03-17 00:11:53 +0800

    updated views.py to function based view render

--- ledger/urls.py
+++ ledger/urls.py
@@ -1,11 +1,12 @@
-from django.urls import path
+from django.urls import path, include
+
 
 
 from .views import recipe_list_view, recipe_detail_view
 
 urlpatterns = [
-    path('recipes/list', recipe_list_view.as_view(), name='ledger'),
-    path('recipe/<int:pk>', recipe_detail_view.as_view(), name='recipe_detail'),
+    path('recipes/list', recipe_list_view, name='ledger'),
+    path('recipe/<int:pk>', recipe_detail_view, name='recipe_detail'),
 ]
 
 app_name = 'ledger'
--- ledger/views.py
+++ ledger/views.py
@@ -1,22 +1,32 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views.generic.list import ListView
-from django.views.generic.detail import DetailView
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
 
 from .models import Ingredient, Recipe, RecipeIngredient
 
 # Create your views here.
 
-class recipe_list_view(ListView):
+def recipe_list_view(request):
+    print(request)
 
-    model = Recipe
-    template_name = 'recipe_list.html'
+    recipes = Recipe.objects.all()
+    ingredients = Ingredient.objects.all()
 
+    ctx = {
+        "Recipes": recipes,
+        "Ingredients": ingredients
+    }
 
-class recipe_detail_view(LoginRequiredMixin, DetailView):
-    
-    model = Recipe
-    template_name = 'recipe_detail.html'
-    redirect_field_name = ''
+    return render(request, "recipe_list.html", ctx)
 
+@login_required
+def recipe_detail_view(request, pk):
+    print(request)
+
+    recipe = Recipe.objects.get(pk = pk)
+
+    ctx = {
+        "Recipe": recipe,
+    }
+
+    return render(request, "recipe_detail.html", ctx)
